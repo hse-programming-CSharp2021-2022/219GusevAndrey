fede74b57a74efc1b729db2ef19c21dfd5585d98
andrey160703 <avgusev_6@edu.hse.ru>
2021-11-05 13:46:42 +0300

    1

---
+++ M2/Seminar2/CW1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+namespace CW1
+{
+    class MyComplex
+    {
+        public double re { get; }
+        public double im { get; }
+
+        public MyComplex(double xre, double xim)
+        {
+            re = xre;
+            im = xim;
+        }
+
+        // Неправильная реализация:
+        //public static MyComplex operator ++(MyComplex mc)
+        //{ mc.re++; mc.im++; return mc; }
+        public static MyComplex operator --(MyComplex mc)
+        {
+            return new MyComplex(mc.re - 1, mc.im - 1);
+        }
+        
+        public static MyComplex operator ++(MyComplex mc)
+        {
+            return new MyComplex(mc.re + 1, mc.im + 1);
+        }
+
+        public double Mod()
+        {
+            return Math.Abs(re * re + im * im);
+        }
+
+        public static bool operator true(MyComplex f)
+        {
+            if (f.Mod() > 1.0) return true;
+            return false;
+        }
+
+        public static bool operator false(MyComplex f)
+        {
+            if (f.Mod() <= 1.0) return true;
+            return false;
+        }
+
+        public static MyComplex operator +(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re + b.re, a.im + b.im);
+        } 
+        
+        public static MyComplex operator -(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re - b.re, a.im - b.im);
+        } 
+        
+        public static MyComplex operator *(MyComplex a, MyComplex b)
+        {
+            return new MyComplex(a.re * b.re - a.im - b.im, a.re * b.re + a.im - b.im);
+        } 
+        
+        public static MyComplex operator /(MyComplex a, MyComplex b)
+        {
+            return new MyComplex((a.re * b.re + a.im - b.im) / (b.re * b.re + b.im * b.im), (b.re * b.re - a.im - a.im) / (b.re * b.re + b.im * b.im));
+        }
+
+        public override string ToString()
+        {
+            return re + " " + im;
+        }
+        
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = new MyComplex(10f, 21f);
+            var b = new MyComplex(8f, 12f);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+        }
+    }
+}
---
+++ M2/Seminar2/HW1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace HW1
+{
+    internal class Program
+    {
+        public class ConsolePlate
+        {
+            private char _plateChar;
+            private ConsoleColor _plateColor = ConsoleColor.White;
+            private ConsoleColor _backgroundColor;
+
+            public char PlateChar
+            {
+                set
+                {
+                    if (value < 'A' || value > 'Z') _plateChar = 'A';
+                    else _plateChar = value;
+                }
+                get { return _plateChar; }
+            }
+
+            public ConsoleColor PlateColor
+            {
+                set
+                {
+                    _plateColor = value;
+                }
+                get
+                {
+                    return _plateColor;
+                }
+            }
+
+            public ConsoleColor BackgroundColor
+            {
+                set
+                {
+                    if (_plateColor == value)
+                        _backgroundColor = (ConsoleColor) (((int) value + 1) % 16);
+                    else
+                        _backgroundColor = value;
+                }
+                get
+                {
+                    return _backgroundColor;
+                }
+            }
+
+            public ConsolePlate()
+            { 
+                PlateChar = 'A';
+            }
+
+            public ConsolePlate(char plateChar, ConsoleColor plateColor, ConsoleColor backgroundColor)
+            {
+                PlateChar = plateChar;
+                PlateColor = plateColor;
+                BackgroundColor = backgroundColor;
+            }
+            
+            public override string ToString()
+            {
+                Console.ForegroundColor = PlateColor;
+                Console.BackgroundColor = BackgroundColor;
+                return PlateChar.ToString();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var arr = new ConsolePlate[1000];
+            var rand = new Random();
+            for (var i = 0; i < 1000; i++)
+            {
+                arr[i] = new ConsolePlate((char)rand.Next(0, 150), (ConsoleColor)rand.Next(0, 16),(ConsoleColor)rand.Next(0, 16));
+                Console.Write(arr[i]);
+            }
+            Console.BackgroundColor = default;
+            Console.ForegroundColor = default;
+            Console.WriteLine();
+            int n = int.Parse(Console.ReadLine());
+
+            ConsolePlate o = new ConsolePlate('O', ConsoleColor.White, ConsoleColor.Magenta);
+            ConsolePlate x = new ConsolePlate('X', ConsoleColor.White, ConsoleColor.Red);
+            
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                        Console.Write(o);
+                    else
+                        Console.Write(x);
+                }
+                Console.WriteLine();
+            }
+            
+
+        }
+    }
+}
