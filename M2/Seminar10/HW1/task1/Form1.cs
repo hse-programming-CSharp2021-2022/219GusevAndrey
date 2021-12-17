5b536823f74f9a532316ded8db0f8c126f34f88b
andrey160703 <avgusev_6@edu.hse.ru>
2021-12-17 21:44:22 +0300

    1

---
+++ M2/Seminar10/HW1/task1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        private int X, Y;
+        private int width, height;
+
+        public Form1()
+        {
+            InitializeComponent();
+            X = 0;
+            Y = 0;
+            width = 250;
+            height = 250;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            trackBar1.Maximum = Width - width;
+            e.Graphics.FillRectangle(new SolidBrush(SystemColors.ControlDark), X, Y, width, height);
+            TransparencyKey = SystemColors.ControlDark;
+         
+            trackBar2.Maximum = Height - height;
+            e.Graphics.FillRectangle(new SolidBrush(SystemColors.ControlDark), X, Y, width, height);
+            TransparencyKey = SystemColors.ControlDark;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            X = trackBar1.Value;
+            Invalidate();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Y = trackBar2.Maximum - trackBar2.Value;
+            Invalidate();
+        }
+
+    }
+}
---
+++ M2/Seminar7/HW1/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+class Person
+{
+    public string Name { get; }
+    public int Age { get; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public virtual string Say()
+    {
+        return "I am a simple person.";
+    }
+
+    public override string ToString()
+    {
+        return Say() + " " + $"My name is {Name}, my age is {Age}.";
+    }
+}
+
+class Doctor : Person
+{
+    private int exp;
+    public int Exp
+    {
+        get => exp;
+        set
+        {
+            if (value >= 5 && value <= 20)
+                exp = value;
+            else
+            {
+                var rand = new Random();
+                exp = rand.Next(5, 21);
+            }
+        }
+    }
+
+    public override string Say()
+    {
+        return $"I am a doctor. I've studied for {exp} years.";
+    }
+
+    public string Heal(Person p)
+    {
+        return $"I'm gonna heal: " + p.ToString();
+    }
+
+    public Doctor(string name, int age, int expr) : base(name, age)
+    {
+        if (expr >= 5 && expr <= 20)
+            exp = expr;
+        else
+        {
+            var rand = new Random();
+            exp = rand.Next(5, 21);
+        }
+    }
+}
+
+class Intern : Doctor
+{
+    private int exp;
+    public int Exp
+    {
+        get { return exp; }
+        set
+        {
+            if (value >= 1 && value <= 4)
+                exp = value;
+            else
+            {
+                var rand = new Random();
+                exp = rand.Next(1, 5);
+            }
+        }
+    }
+
+    public override string Say()
+    {
+        return $"I am an intern. I've studied for {exp} years.";
+    }
+
+    public Intern(string name, int age, int expr) : base(name, age, expr)
+    {
+        if (expr >= 1 && expr <= 4)
+            exp = expr;
+        else
+        {
+            var rand = new Random();
+            exp = rand.Next(1, 4);
+        }
+    }
+}
+
+class Program
+{
+    static Random rand = new Random();
+
+    public static string GenerateName()
+    {
+        var n = rand.Next(3, 11);
+        var s = new StringBuilder(n);
+        s.Append((char)rand.Next('A', 'Z' + 1));
+        for (var i = 1; i < n; i++)
+            s.Append((char)rand.Next('a', 'z' + 1));
+        return s.ToString();
+    }
+
+    public static void Main(string[] args)
+    {
+        var dr = new Doctor(GenerateName(), rand.Next(18, 61), rand.Next(-50, 51));
+        var intern = new Intern(GenerateName(), rand.Next(18, 61), rand.Next(-50, 51));
+
+        var n = int.Parse(Console.ReadLine());
+        var m = new Person[n];
+
+        for (var i = 0; i < n - 1; i++)
+            m[i] = new Person(GenerateName(), rand.Next(120));
+        m[n - 1] = new Doctor(GenerateName(), rand.Next(18, 61), rand.Next(-50, 51));
+
+        for (int i = 0; i < n / 2; i++)
+            Console.WriteLine(dr.Heal(m[i]));
+        Console.WriteLine(intern.ToString());
+        for (int i = n / 2; i < (n % 2 == 0 ? n : n - 1); i++)
+            Console.WriteLine(dr.Heal(m[i]));
+    }
+}
